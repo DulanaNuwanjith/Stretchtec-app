853d82dc1e33da5b971a9dfc69c52d279a0baf3b
DulanaNuwanjith99 <dulana@hsenidlanka.com>
2025-07-01 11:07:10 +0530

    fix issus and create add user ui

--- Stretchtec/resources/views/store-management/storeManagement.blade.php
+++ Stretchtec/resources/views/store-management/storeManagement.blade.php
@@ -1,6 +1,6 @@
 <div class="flex h-full w-full font-sans">
     @include('layouts.side-bar')
-    <div class="flex-1 overflow-y-auto p-8 bg-gray-50 dark:bg-gray-900">
+    <div class="flex-1 overflow-y-auto p-8 bg-white dark:bg-gray-900">
         <!-- Header -->
         <div class="flex justify-between items-center mb-8">
             <h2 class="text-3xl font-bold text-gray-800 dark:text-white">Store Management</h2>
