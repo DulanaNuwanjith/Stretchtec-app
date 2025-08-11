01b1e8ab2cf713cf6b642a6ee6aea2ebb899448e
DulanaNuwanjith99 <dulana@hsenidlanka.com>
2025-08-11 17:09:34 +0530

    text change

--- Stretchtec/resources/views/sample-development/pages/sample-inquery-details.blade.php
+++ Stretchtec/resources/views/sample-development/pages/sample-inquery-details.blade.php
@@ -1105,7 +1105,7 @@
                                                                         onclick="selectDropdownOptionItemAdd(this, 'Cord', 'item')">Cord</button>
                                                                     <button type="button"
                                                                         class="dropdown-option w-full text-left px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600"
-                                                                        onclick="selectDropdownOptionItemAdd(this, 'Tape', 'item')">Twill
+                                                                        onclick="selectDropdownOptionItemAdd(this, 'Twill Tape', 'item')">Twill
                                                                         Tape</button>
                                                                 </div>
                                                             </div>
