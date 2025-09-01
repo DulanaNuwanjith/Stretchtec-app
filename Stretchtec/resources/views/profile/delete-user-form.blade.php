9a63c5d3f3b5a9ba6d7eed41bf706fe71fe6e307
Amika Subasinghe <amixx2005@gmail.com>
2025-09-01 09:08:16 +0530

    code cleanup profile pages

--- Stretchtec/resources/views/profile/delete-user-form.blade.php
+++ Stretchtec/resources/views/profile/delete-user-form.blade.php
@@ -13,9 +13,12 @@
         </div>
 
         <div class="mt-5">
-            <div class="bg-yellow-50 border border-yellow-300 text-yellow-800 px-4 py-3 rounded-lg flex items-start shadow-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mt-0.5 mr-3 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M12 2a10 10 0 100 20 10 10 0 000-20z" />
+            <div
+                class="bg-yellow-50 border border-yellow-300 text-yellow-800 px-4 py-3 rounded-lg flex items-start shadow-sm">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mt-0.5 mr-3 text-yellow-500" fill="none"
+                     viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M13 16h-1v-4h-1m1-4h.01M12 2a10 10 0 100 20 10 10 0 000-20z"/>
                 </svg>
                 <span class="text-sm leading-5">
                     {{ __('Your account cannot be deleted directly. Please reach out to your administrator') }}
