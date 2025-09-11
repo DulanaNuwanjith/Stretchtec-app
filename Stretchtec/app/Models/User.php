8f15849b773182581064e89a069c6ebe54bc76e1
Amika Subasinghe <amixx2005@gmail.com>
2025-09-11 09:38:45 +0530

    usermanagement controller code cleaup and warning sign remove

--- Stretchtec/app/Models/User.php
+++ Stretchtec/app/Models/User.php
@@ -10,6 +10,10 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @method static paginate(int $int)
+ * @method static findOrFail($id)
+ */
 class User extends Authenticatable
 {
     use HasApiTokens;
