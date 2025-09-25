8ec771fefeb2213b5f74f8b53d6fac995b9dc858
Amika Subasinghe <amixx2005@gmail.com>
2025-09-25 15:36:25 +0530

    fields added for creating a new production inquiry

--- Stretchtec/app/Models/ProductInquiry.php
+++ Stretchtec/app/Models/ProductInquiry.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductInquiry extends Model
 {
-    //
+    protected $fillable = [
+        'prod_order_no',
+        'po_received_date',
+        'customer_name',
+        'merchandiser_name',
+        'po_number',
+        'size',
+        'item',
+        'color',
+        'reference_no',
+        'shade',
+        'tkt',
+        'qty',
+        'uom',
+        'price',
+        'customer_req_date',
+        'our_prod_date',
+        'stock_qty',
+        'to_make_qty',
+        'delivered_qty',
+        'in_production_qty',
+        'balance_qty',
+        'invoice_no',
+        'invoice_date',
+        'isSentToStock',
+        'isSentToProduction',
+        'status',
+        'remarks',
+    ];
 }
--- Stretchtec/database/migrations/2025_09_22_141741_create_product_inquiries_table.php
+++ Stretchtec/database/migrations/2025_09_22_141741_create_product_inquiries_table.php
@@ -4,15 +4,41 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('product_inquiries', function (Blueprint $table) {
+        Schema::create('product_inquiries', static function (Blueprint $table) {
             $table->id();
+            $table->string('prod_order_no');
+            $table->dateTime('po_received_date');
+            $table->string('customer_name');
+            $table->string('merchandiser_name');
+            $table->string('po_number');
+            $table->string('size');
+            $table->string('item');
+            $table->string('color');
+            $table->string('reference_no');
+            $table->string('shade');
+            $table->string('tkt');
+            $table->decimal('qty');
+            $table->string('uom');
+            $table->decimal('price');
+            $table->date('customer_req_date');
+            $table->date('our_prod_date');
+            $table->decimal('stock_qty')->nullable();
+            $table->decimal('to_make_qty')->nullable();
+            $table->decimal('delivered_qty')->nullable();
+            $table->decimal('in_production_qty')->nullable();
+            $table->decimal('balance_qty')->nullable();
+            $table->string('invoice_no')->nullable();
+            $table->date('invoice_date')->nullable();
+            $table->boolean('isSentToStock')->nullable();
+            $table->boolean('isSentToProduction')->default(false);
+            $table->string('status')->default('Pending');
+            $table->string('remarks')->nullable();
             $table->timestamps();
         });
     }
