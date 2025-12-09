9c8bb5d83ab90e9224729a52ff06f88a42feafed
Amika Subasinghe <amixx2005@gmail.com>
2025-12-09 14:34:09 +0530

    Add models, controllers, and migrations for section orders
    Introduces BraidingSectionOrders, KnittedSectionOrders, and LoomSectionOrders models and their corresponding resource controllers. Adds migrations to create the respective tables in the database.

---
+++ Stretchtec/app/Http/Controllers/KnittedSectionOrdersController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KnittedSectionOrders;
+use Illuminate\Http\Request;
+
+class KnittedSectionOrdersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KnittedSectionOrders $knittedSectionOrders)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KnittedSectionOrders $knittedSectionOrders)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, KnittedSectionOrders $knittedSectionOrders)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KnittedSectionOrders $knittedSectionOrders)
+    {
+        //
+    }
+}
