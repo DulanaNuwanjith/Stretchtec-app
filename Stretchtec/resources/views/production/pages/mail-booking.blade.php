66b7b1e2debf1f389bce1aef3e2d517152a19c19
DulanaNuwanjith99 <90448857+DulanaNuwanjith@users.noreply.github.com>
2025-12-12 10:35:43 +0530

    column width change

--- Stretchtec/resources/views/production/pages/mail-booking.blade.php
+++ Stretchtec/resources/views/production/pages/mail-booking.blade.php
@@ -557,7 +557,7 @@
                                                 Requested Date
                                             </th>
                                             <th
-                                                class="font-bold sticky top-0 bg-gray-200 dark:bg-gray-700 px-4 py-3 w-32 text-xs text-gray-600 dark:text-gray-300 uppercase whitespace-normal break-words">
+                                                class="font-bold sticky top-0 bg-gray-200 dark:bg-gray-700 px-4 py-3 w-48 text-xs text-gray-600 dark:text-gray-300 uppercase whitespace-normal break-words">
                                                 Production Deadline
                                             </th>
                                             <th
